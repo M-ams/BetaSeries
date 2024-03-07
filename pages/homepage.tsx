4bc5ac83ea63ac1c6626f6a7703f8b3a779dabde
Mams-sy <93870019+Mams-sy@users.noreply.github.com>
2024-03-07 19:11:09 +0100

    upgrade slider

--- pages/homepage.tsx
+++ pages/homepage.tsx
@@ -1,4 +1,12 @@
-import { Box, Heading, Grid, Image, Link, Button } from "@chakra-ui/react";
+import {
+  Box,
+  Heading,
+  Grid,
+  Image,
+  Link,
+  Button,
+  Flex,
+} from "@chakra-ui/react";
 import Navbar from "../components/Navbar";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
@@ -16,7 +24,54 @@ interface Series {
   title: string;
 }
 
-const SERIES_PER_PAGE = 100;
+const SERIES_PER_PAGE = 10;
+
+function SeriesWithNumber({
+  series,
+  index,
+}: {
+  series: Series;
+  index: number;
+}) {
+  const right = index < 9 ? "40%" : "3%";
+
+  return (
+    <Flex
+      key={series.id}
+      className="relative flex flex-row items-center justify-center"
+      mt={"8vh"}
+      mb={"8vh"}
+    >
+      <Box
+        color="white"
+        fontWeight="bold"
+        fontSize="250px"
+        zIndex={0}
+        position="absolute"
+        right={right}
+        bottom={"-40%"}
+        fontStyle={"bold"}
+      >
+        {index + 1}
+      </Box>
+      <Box position="relative" w="90%">
+        <Link href={`/series/${series.id}`} textDecoration="none">
+          <Image
+            src={series.images.poster || ""}
+            alt={series.title}
+            w="50%"
+            h="auto"
+            maxH="600px"
+            borderRadius="md"
+            marginLeft={"40%"}
+            _hover={{ transform: "scale(1.05)" }}
+            transition="transform 0.2s ease-in-out"
+          />
+        </Link>
+      </Box>
+    </Flex>
+  );
+}
 
 function Homepage() {
   const [seriesList, setSeriesList] = useState<Series[]>([]);
@@ -52,11 +107,11 @@ function Homepage() {
 
   const settings = {
     dots: true,
+    dotsClass: "slick-dots",
     speed: 2000,
-    slidesToShow: 9,
-    slidesToScroll: 9,
-    
-   
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    infinite: true,
   };
 
   return (
@@ -74,27 +129,10 @@ function Homepage() {
         Séries populaires
       </Heading>
 
-      <Box width="90vw" mx={"auto"}>
-        <Slider {...settings}>
-          {currentSeries.map((series) => (
-            <Box
-              key={series.id}
-              className="flex flex-col items-center justify-center"
-            >
-              <Link href={`/series/${series.id}`} textDecoration="none">
-                <Image
-                  src={series.images.poster || ""}
-                  alt={series.title}
-                  w="90%"
-                  h="auto"
-                  maxH="600px" // Ajustez la hauteur maximale selon vos besoins
-                  borderRadius="md"
-                  transition="transform 0.2s"
-                  _hover={{ transform: "scale(1.05)" }}
-                  m={2}
-                />
-              </Link>
-            </Box>
+      <Box width="90vw" m={"auto"} mt={"5vh"}>
+        <Slider className="mt-[vh]" {...settings}>
+          {currentSeries.map((series, index) => (
+            <SeriesWithNumber key={series.id} series={series} index={index} />
           ))}
         </Slider>
       </Box>
